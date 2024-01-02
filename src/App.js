17ae97564da622248e1f0256f30e89fe3f10d130
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2024-01-02 18:48:25 +0530

    final project

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Search from './components/search/Search';
 import Weather from './components/weather/Weather';
 import Forecast from './components/forecast/forecast';
 import { WEATHER_API_URL, FORECAST_API_URL, WEATHER_API_KEY } from './components/api';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Logo from './logo.png';
+import Footer from './components/Footer';
+
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -13,35 +15,101 @@ function App() {
   const handleOnsearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-    const forecastFetch = fetch(`${FORECAST_API_URL
-      }/?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
-    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
-      const weatherResponse = await response[0].json();
-      const forecastResponse = await response[1].json();
-      setWeather({ city: searchData.label, ...weatherResponse });
-      setForecast({ city: searchData.label, ...forecastResponse });
-      let forecastDates = JSON.parse(JSON.stringify(forecastResponse));
-      const weatherArrayByDate = Object.values(forecastDates.list).reduce((result, details) => {
-        const date = details.dt_txt.split(' ')[0];
-        if (!result[date]) {
-          result[date] = [];
+    const forecastFetch = fetch(`${FORECAST_API_URL}/?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setWeather({ city: searchData.label, ...weatherResponse });
+        let forecastDates = JSON.parse(JSON.stringify(forecastResponse));
+        console.log(forecastDates);
+        const extractArrayByDate = (property) => {
+          return Object.values(forecastDates.list).reduce((result, details) => {
+            const date = details.dt_txt.split(' ')[0];
+            if (!result[date]) {
+              result[date] = [];
+            }
+            if (property === 'main.temp')
+              result[date].push(details.main.temp);
+            if (property === 'main.humidity')
+              result[date].push(details.main.humidity);
+            if (property === 'main.pressure')
+              result[date].push(details.main.pressure);
+            if (property === 'wind.speed')
+              result[date].push(details.wind.speed);
+              if (property === 'weather.description')
+              result[date].push(details.weather[0].description);
+              if (property === 'weather.icon')
+              result[date].push(details.weather[0].icon);
+            return result;
+          }, {});
+        };
+        const temperatureArrayByDate = extractArrayByDate('main.temp');
+        const humidityArrayByDate = extractArrayByDate('main.humidity');
+        const pressureArrayByDate = extractArrayByDate('main.pressure');
+        const windArrayByDate = extractArrayByDate('wind.speed');
+        const weatherDescArrayByDate = extractArrayByDate('weather.description');
+        const weatherIconArrayByDate = extractArrayByDate('weather.icon');
+        function calculateAverage(values) {
+          if (values.length === 0) {
+            return 0;
+          }
+          const sum = values.reduce((acc, val) => acc + val, 0);
+          const average = sum / values.length;
+          return average;
         }
-        result[date].push(details.main.temp);
-        return result;
-      }, {});
-      Object.keys(weatherArrayByDate).forEach(date => {
-        weatherArrayByDate[date] = { min: Math.min(...weatherArrayByDate[date]), max:Math.max(...weatherArrayByDate[date])};
-      });
-      console.log(weatherArrayByDate);
-    })
+        function getMaxOccurrencesDateWise(data) {
+          const result = {};
+          for (const [date, weatherArray] of Object.entries(data)) {
+            const occurrencesMap = {};
+            for (const weatherType of weatherArray) {
+              occurrencesMap[weatherType] = (occurrencesMap[weatherType] || 0) + 1;
+            }
+            let maxWeatherType;
+            let maxCount = 0;
+            for (const [weatherType, count] of Object.entries(occurrencesMap)) {
+              if (count > maxCount) {
+                maxWeatherType = weatherType;
+                maxCount = count;
+              }
+            }
+            result[date] =maxWeatherType;
+          }
+          return result;
+        }
+        const weatherDescByDate=getMaxOccurrencesDateWise(weatherDescArrayByDate);
+        const weatherIconByDate=getMaxOccurrencesDateWise(weatherIconArrayByDate);
+        let weatherData = {};
+        Object.keys(temperatureArrayByDate).forEach(date => {
+          weatherData[date] = {
+            min: Math.round(Math.min(...temperatureArrayByDate[date])),
+            max: Math.round(Math.max(...temperatureArrayByDate[date])),
+            wind: Math.round(calculateAverage(windArrayByDate[date])),
+            humidity: Math.round(calculateAverage(humidityArrayByDate[date])),
+            pressure: Math.round(calculateAverage(pressureArrayByDate[date])),
+            description:weatherDescByDate[date],
+            icon:weatherIconByDate[date]
+          };
+        });
+        let today = new Date();
+        today = today.toISOString().split('T')[0];
+        delete weatherData[today];
+        setForecast({ city: searchData.label, weatherData });
+      })
       .catch((err) => console.log(err));
   }
+
+  useEffect(() => {
+    console.log(forecast);
+  }, [forecast]);
+
   return (
     <div className="container">
       <img src={Logo} alt="logo" />
       <Search onSearchChange={handleOnsearchChange} />
       {weather && <Weather data={weather} />}
       {forecast && <Forecast data={forecast} />}
+      <Footer/>
     </div>
   );
 }
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,69 +1,69 @@
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import "./forecast.css";
 
 const WeekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
 const Forecast = ({ data }) => {
     const dayInWeek = new Date().getDay();
     const forecastDays = WeekDays.slice(dayInWeek, WeekDays.length).concat(WeekDays.slice(0, dayInWeek));
+    let i = 0;
     return (
         <>
-        <h2>Next 7 Days</h2>
-            {data.list.slice(0, 7).map((item, idx) => (
-                <Accordion className="accordion" key={idx}>
+            <h2>Next 5 Days</h2>
+            {data && data.weatherData && Object.entries(data.weatherData).map(([date, item]) => (
+                <Accordion className="accordion" key={date}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon className="expand-more-icon" />}
                         aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    ><div><img alt="forecast" className="forecast-icon" src={`icons/${item.weather[0].icon}.png`} /></div>
-                        <Typography className="weekday">
-                            <div className='weekday-label'>{forecastDays[idx]} -  {item.dt_txt}</div>
-                            <div className='weekday-description'>{item.weather[0].description}</div>
-                            <div className='weekday-min-max-temp'>{Math.round(item.main.temp_max)}°C / {Math.round(item.main.temp_min)}°C </div>
-                        </Typography>
+                        id="panel1a-header">
+                        <div><img alt="forecast" className="forecast-icon" src={`icons/${item.icon}.png`} /></div>
+                        <div className="weekday">
+                            <div><p className='weekday-date'>{new Date(date).toLocaleDateString('en-GB', { day: 'numeric', month: 'numeric' })}</p>
+                                <p className='weekday-label'>{forecastDays[i++]}</p></div>
+
+                            <div className='weekday-description'>{item.description}</div>
+                            <div className='weekday-min-max-temp'>{item.max}°C / {item.min}°C </div>
+                        </div>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>
-                            <div className="bottom">
-                                <div>
-                                    <div className="parameter-row">
-                                        <span className="parameter-label">Feels Like</span>
-                                        <span className="parameter-value">{Math.round(item.main.temp)}°C</span>
-                                    </div>
-                                    <div className="parameter-row">
-                                        <span className="parameter-label">Wind</span>
-                                        <span className="parameter-value">{item.wind.speed} m/s</span>
-                                    </div>
+                        <div className="bottom">
+                            <div>
+                            <div className="parameter-row">
+                                    <span className="parameter-label">Forecast</span>
+                                    <span className="parameter-value">{item.description}</span>
                                 </div>
-                                <div>
                                 <div className="parameter-row">
-                                        <span className="parameter-label">Humidity</span>
-                                        <span className="parameter-value">{item.main.humidity}%</span>
-                                    </div>
-                                    <div className="parameter-row">
-                                        <span className="parameter-label">Pressure</span>
-                                        <span className="parameter-value">{item.main.pressure} hPa</span>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="parameter-row">
-                                        <span className="parameter-label">Min Temperature</span>
-                                        <span className="parameter-value">{Math.round(item.main.temp_min)}°C</span>
-                                    </div>
-                                    <div className="parameter-row">
-                                        <span className="parameter-label">Max temperature</span>
-                                        <span className="parameter-value">{Math.round(item.main.temp_max)}°C</span>
-                                    </div>
-
+                                    <span className="parameter-label">Wind</span>
+                                    <span className="parameter-value">{item.wind} m/s</span>
                                 </div>
                             </div>
-                        </Typography>
+                            <div>
+                                <div className="parameter-row">
+                                    <span className="parameter-label">Humidity</span>
+                                    <span className="parameter-value">{item.humidity}%</span>
+                                </div>
+                                <div className="parameter-row">
+                                    <span className="parameter-label">Pressure</span>
+                                    <span className="parameter-value">{item.pressure} hPa</span>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="parameter-row">
+                                    <span className="parameter-label">Min Temperature</span>
+                                    <span className="parameter-value">{Math.round(item.min)}°C</span>
+                                </div>
+                                <div className="parameter-row">
+                                    <span className="parameter-label">Max temperature</span>
+                                    <span className="parameter-value">{Math.round(item.max)}°C</span>
+                                </div>
+
+                            </div>
+                        </div>
                     </AccordionDetails>
-                </Accordion>))}
+                </Accordion>
+            ))}
         </>
     );
 }
